9d6d0633633e4647905be04db4ffe830cd8b6bef
Michaël Marinetti <github@marinetti.fr>
2017-03-20 17:06:21 +0100

    Fix: redmine plugin migrate sql error

--- db/migrate/003_add_trello_board_mapping_to_project.rb
+++ db/migrate/003_add_trello_board_mapping_to_project.rb
@@ -1,6 +1,6 @@
 class AddTrelloBoardMappingToProject < ActiveRecord::Migration
   def change
-    add_column :projects, :trello_mapping_redmine_statuses, :text, :default => "", :null => false
-    add_column :projects, :trello_mapping_trello_lists,     :text, :default => "", :null => false
+    add_column :projects, :trello_mapping_redmine_statuses, :text, :null => false
+    add_column :projects, :trello_mapping_trello_lists,     :text, :null => false
   end
 end
