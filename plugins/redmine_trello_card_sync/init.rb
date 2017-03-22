57ebb8debbfe465df276633a1692ca654f35fdc9
Hiroshi Yui <hiroshiyui@kkbox.com>
2017-03-22 09:50:31 +0800

    Bump 0.0.5 release
    Signed-off-by: Hiroshi Yui <hiroshiyui@kkbox.com>

--- plugins/redmine_trello_card_sync/init.rb
+++ plugins/redmine_trello_card_sync/init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_trello_card_sync do
   name 'Trello card sync plugin'
   author 'Hiroshi Yui'
   description 'Sync Redmine ticket to Trello card'
-  version '0.0.4'
+  version '0.0.5'
   url 'https://github.com/hiroshiyui/redmine_trello_card_sync'
   author_url 'https://ghostsinthelab.org/'
   requires_redmine :version_or_higher => '2.3.2'
