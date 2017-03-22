679a72c4432291933734322ecdbe6b52406da854
Hiroshi Yui <hiroshiyui@kkbox.com>
2017-03-22 09:50:04 +0800

    Fix DB migration to improve compatibility across DB products
    Signed-off-by: Hiroshi Yui <hiroshiyui@kkbox.com>

--- plugins/redmine_trello_card_sync/db/migrate/003_add_trello_board_mapping_to_project.rb
+++ plugins/redmine_trello_card_sync/db/migrate/003_add_trello_board_mapping_to_project.rb
@@ -1,6 +1,6 @@
 class AddTrelloBoardMappingToProject < ActiveRecord::Migration
   def change
-    add_column :projects, :trello_mapping_redmine_statuses, :text, :null => false
-    add_column :projects, :trello_mapping_trello_lists,     :text, :null => false
+    add_column :projects, :trello_mapping_redmine_statuses, :text, :default => nil
+    add_column :projects, :trello_mapping_trello_lists,     :text, :default => nil
   end
 end
