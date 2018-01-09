c7d9ee16c83127dd32aaca66f0512fcbe42e058d
Hiroshi Yui <hiroshiyui@kkbox.com>
2018-01-09 17:14:22 +0800

    Fix MySQL text field default value migrations
    Signed-off-by: Hiroshi Yui <hiroshiyui@kkbox.com>

--- db/migrate/005_change_trello_exclude_tracker_default_to_project.rb
+++ db/migrate/005_change_trello_exclude_tracker_default_to_project.rb
@@ -1,5 +1,5 @@
 class ChangeTrelloExcludeTrackerDefaultToProject < ActiveRecord::Migration
   def change
-    change_column :projects, :trello_excluded_trackers, :text, default: '[]'
+    change_column :projects, :trello_excluded_trackers, :text
   end
 end
--- db/migrate/007_add_trello_list_mapping_to_project.rb
+++ db/migrate/007_add_trello_list_mapping_to_project.rb
@@ -1,5 +1,11 @@
 class AddTrelloListMappingToProject < ActiveRecord::Migration
   def change
-    add_column :projects, :trello_list_mapping, :text, :default => "", :null => false
+    add_column :projects, :trello_list_mapping, :text
+
+    # set default value in MySQL, ref: https://github.com/KKBOX/redmine-trello-card-sync/issues/16
+    Project.all.each do |p|
+      p.trello_list_mapping = ""
+      p.save!
+    end
   end
 end
